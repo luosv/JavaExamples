3018405bf668a3a9bd4b57fc0e6df37162315ff8
Luosw <luo_shaowei@outlook.com>
2016-11-17 19:22:23 +0800

    线程挂起实例

--- JavaExamples/src/javaThread/SleepingThread.java
+++ JavaExamples/src/javaThread/SleepingThread.java
@@ -1,7 +1,45 @@
 package javaThread;
 
 /**
+ * 线程挂起
  * Created by lv on 2016/11/17 0017.
  */
-public class SleepingThread {
+public class SleepingThread extends Thread {
+
+    private int countDown = 5;
+    private static int threadCount = 0;
+
+    private SleepingThread() {
+        super("" + ++threadCount);
+        start();
+    }
+
+    public String toString() {
+        return "#" + getName() + ": " + countDown;
+    }
+
+    public void run() {
+        while (true) {
+            System.out.println(this);
+            if (--countDown == 0) {
+                return;
+            }
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i < 5; i++) {
+            //new SleepingThread().join();
+        }
+        new SleepingThread().join();
+        System.out.println("线程已被挂起");
+
+    }
+
 }
