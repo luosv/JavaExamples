a538d1db60947248fce730d5b35e90d8ada8f50e
Luosw <luo_shaowei@outlook.com>
2017-02-06 12:02:12 +0800

    format GetStack

--- JavaExamples/src/other/GetStack.java
+++ JavaExamples/src/other/GetStack.java
@@ -13,7 +13,8 @@ public class GetStack {
         StringBuilder buffer = new StringBuilder(); // 单线程用StringBuilder速度快、多线程用StringBuffer线程安全
         buffer.append("\n");
         for (StackTraceElement stackTraceElement : stackTrace) {
-            buffer.append(stackTraceElement.getClassName()).append(".").append(stackTraceElement.getMethodName()).append(".").append(stackTraceElement.getLineNumber()).append("\n");
+            buffer.append(stackTraceElement.getClassName()).append(".").append(stackTraceElement.getMethodName())
+                    .append(".").append(stackTraceElement.getLineNumber()).append("\n");
         }
         return buffer.toString();
 
